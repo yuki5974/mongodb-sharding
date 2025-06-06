9b70e0d99d34001486f10d71337b4f56e1d2422d
Yuki Murakami <YUMURA@jp.ibm.com>
2025-06-06 12:59:25 +0900

    add mongos

---
+++ initmongos.js
@@ -0,0 +1,2 @@
+sh.addShard("rs1/host.docker.internal:10001,host.docker.internal:10002,host.docker.internal:10003")
+sh.addShard("rs2/host.docker.internal:20001,host.docker.internal:20002,host.docker.internal:20003")
